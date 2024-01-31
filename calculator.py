bf730f8786b8ed26d3fdc78007caf255a93bcd51
MhlifiliAnako <158313167+MhlifiliAnako@users.noreply.github.com>
2024-01-31 20:11:30 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# display a welcome message
+print("TIP CALCULATOR")
+print()
+
+# get input from the user
+meal_cost = float(input("Cost of meal: "))
+tip_percent = float(input("Tip percent:  "))
+print()
+
+# calculate tip and total amount
+tip_amount = round(meal_cost * (tip_percent / 100), 2)
+total = round(meal_cost + tip_amount, 2)
+
+# display the results
+print("Tip amount:  ", tip_amount)
+print("Total amount:", total)
